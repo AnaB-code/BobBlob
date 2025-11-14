44734ebdcde10c7ec2035e403c3f090e4fc000e1
AwesumIndustrys <tryst2020@gmail.com>
2025-11-13 19:24:18 -0600

    rudimentary sound system
    TO DO: replace temp sounds with real ones

--- Assets/Scripts/menuButton.cs
+++ Assets/Scripts/menuButton.cs
@@ -2,13 +2,21 @@
 
 public class menuButton : MonoBehaviour {
     public string sceneToLoad;
+    public AudioClip clickSound;
+    public AudioSource audioSource;
 
     public void OnClick() {
-        Debug.Log("You clicked me! You should be loading " + sceneToLoad);
-        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+        audioSource.PlayOneShot(clickSound);
+        if (sceneToLoad != "") {
+            Debug.Log("You clicked me! You should be loading " + sceneToLoad);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+        } else {
+            Debug.Log("No scene has been programmed into this button. Please fix that");
+        }
     }
 
     public void QuitGame() {
+        audioSource.PlayOneShot(clickSound);
         Debug.Log("Bye bye!");
         Application.Quit();
     }
--- Assets/Scripts/pauseScript.cs
+++ Assets/Scripts/pauseScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class pauseScript : MonoBehaviour {
     public GameObject pauseText;
     bool isPaused;
+    public AudioSource audioSource;
+    public AudioSource playerAudioSource;
     
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -15,9 +17,13 @@ public class pauseScript : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.Backspace)) {
                 UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
             }
+            audioSource.Pause();
+            playerAudioSource.Pause();
         } else {
             pauseText.SetActive(false);
             Time.timeScale = 1;
+            audioSource.UnPause();
+            playerAudioSource.UnPause();
         }
     }
 }
