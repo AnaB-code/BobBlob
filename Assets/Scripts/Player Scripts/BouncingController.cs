70688a0112ba171cd740413a26f609a01b0bf8a1
Aj58935 <ajerwi1@ilstu.edu>
2026-02-24 19:07:11 -0600

    Redone Jumping
    hope it works

--- Assets/Scripts/Player Scripts/BouncingController.cs
+++ Assets/Scripts/Player Scripts/BouncingController.cs
@@ -1,54 +1,63 @@
+using System.Diagnostics;
 using UnityEngine;
 
-public class BouncingController : MonoBehaviour {
+public class BouncingController : MonoBehaviour
+{
     public Rigidbody2D player;
     public PlayerMove pm;
-    public float bouncy = 1;
+    public float bouncy = 0;
     public GameObject surface1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {
-        player.sharedMaterial.bounciness = 0;
-        player.sharedMaterial = player.sharedMaterial;
+    void Start()
+    {
+        // player.sharedMaterial.bounciness = 0;
+        // player.sharedMaterial = player.sharedMaterial;
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         //Debug.Log(bouncy);
     }
 
-    public void SetCurrentGround(Collision2D other)
-    {
-        if (surface1 == null)
+    /*
+        public void SetCurrentGround(Collision2D other)
         {
-            surface1 = other.gameObject;
-            bouncy = 1;
-            player.sharedMaterial.bounciness = bouncy;
-            player.sharedMaterial = player.sharedMaterial;
-        }
-        else if (surface1 == other.gameObject)
-        {
-            Bounce(other);
-        }
-        else if (surface1 != other.gameObject)
-        {
-            surface1 = other.gameObject;
-            bouncy = 1;
-            player.sharedMaterial.bounciness = bouncy;
-            player.sharedMaterial = player.sharedMaterial;
-        }
-    }
+            if (surface1 == null)
+            {
+                surface1 = other.gameObject;
+                bouncy = 1f;
+                player.sharedMaterial.bounciness = bouncy;
+                player.sharedMaterial = player.sharedMaterial;
+            }
+            else if (surface1 == other.gameObject)
+            {
+                Bounce();
 
-    public void Bounce(Collision2D other)
+            }
+            else if (surface1 != other.gameObject)
+            {
+                surface1 = other.gameObject;
+                bouncy = 1f;
+                player.sharedMaterial.bounciness = bouncy;
+                player.sharedMaterial = player.sharedMaterial;
+            }
+        }
+        */
+
+    public void Bounce()
     {
         bouncy -= 0.1f;
-
-        if (bouncy < 0.2) {
+        print("Bounce" + bouncy);
+        if (bouncy < 0.2)
+        {
             bouncy = 0;
             pm.OnGround(true);
+            print("Bounce ended");
         }
 
-        player.sharedMaterial.bounciness = bouncy;
-        player.sharedMaterial = player.sharedMaterial;
+        // player.sharedMaterial.bounciness = bouncy;
+        // player.sharedMaterial = player.sharedMaterial;
     }
 }
