6d6c0ef8453f4931686c08d253f16c9ac25cc2f1
vjbolto <vjbolto@ilstu.edu>
2026-03-03 12:07:33 -0600

    levelchanges

---
+++ Assets/Scripts/FloorOpen.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class FloorOpen : MonoBehaviour
+{
+
+    public int counter = 0;
+    public int maxCount = 3;
+
+    Vector3 myPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myPosition=transform.position;
+        print(myPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addCount(int amount) {
+        counter=counter+amount;
+        if(counter>=maxCount) {
+            print("done");
+            // do something
+           transform.position = myPosition+ new Vector3(0,-2.5f,0);
+        } else {
+            print("not done");
+            // do something
+            transform.position = myPosition;        }
+    }
+}
