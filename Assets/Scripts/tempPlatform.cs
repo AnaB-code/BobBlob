38da0093f68cfa47897f5511d802e221b483e4be
AwesumIndustrys <tryst2020@gmail.com>
2026-03-31 19:32:07 -0500

    Bubbles n Birds
    blub blub

--- Assets/Scripts/tempPlatform.cs
+++ Assets/Scripts/tempPlatform.cs
@@ -2,14 +2,18 @@
 
 public class tempPlatform : MonoBehaviour {
     public float lifetime;
+    public Vector2 velocity;
+    public GameObject sourceBird;
     void Start() {
         lifetime = 5f;
     }
 
     void Update() {
         if (lifetime < 0) {
+            sourceBird.GetComponent<bubbleBird>().bubbleExists = false;
             Destroy(this.gameObject);
         }
+        transform.position = Vector2.Lerp(transform.position, velocity, Time.deltaTime);
     }
 
     void OnCollisionStay2D(Collision2D other) {
