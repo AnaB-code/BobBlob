5e44729e265283e9bd6bffec6faa93692dc8b81a
Aj58935 <ajerwi1@ilstu.edu>
2026-04-14 17:23:15 -0500

    level

--- Assets/Scripts/Leo Scripts/SceneChange.cs
+++ Assets/Scripts/Leo Scripts/SceneChange.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour {
-    [SerializeField] private string scene;
-    
-    private void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.CompareTag("Player")){
-            SceneManager.LoadScene(scene);
+    //[SerializeField] private string scene;
+    public int nextSceneLoad;
+
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex +1; 
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextSceneLoad);
+
+            if(nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+            {
+                PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+            }
         }
     }
+    
+   // private void OnCollisionEnter2D(Collision2D other){
+       // if(other.gameObject.CompareTag("Player")){
+            //SceneManager.LoadScene(scene);
+            //if(LoadScene)
+        //}
+   // }
 }
